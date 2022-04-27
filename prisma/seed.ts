545fe7beef2a7dee37cad333a75944223aa0a8cc
huyqbui <shuyttea@gmail.com>
2022-04-26 17:03:59 -0700

    update seed script to create user and playlist

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,7 @@ import { artistsData } from './songsData';
 
 const prisma = new PrismaClient();
 
+// a script that runs only when we execute the seed command, then stops
 const run = async () => {
   await Promise.all(
     artistsData.map(async (artist) => {
@@ -33,6 +34,25 @@ const run = async () => {
       password: bcrypt.hashSync('password', salt),
     },
   });
+
+  const getSongs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map((_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: getSongs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      });
+    })
+  );
 };
 
 run()
